98b467cd5fd38ce1e1de9dfe6f63373bbb515b39
Jags <jagadeeshmurali@gmail.com>
2020-07-28 00:51:49 +0530

    Updates

---
+++ precache-manifest.bf9299fc4520921b732f45ba4ad16239.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab68222da1e9c9803c37d1138dba014",
+    "url": "/Jags-byte/first-app/index.html"
+  },
+  {
+    "revision": "d64578d515efd263bda3",
+    "url": "/Jags-byte/first-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49b5286d487094d1a669",
+    "url": "/Jags-byte/first-app/static/js/2.b8eb2fb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jags-byte/first-app/static/js/2.b8eb2fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64578d515efd263bda3",
+    "url": "/Jags-byte/first-app/static/js/main.620541ce.chunk.js"
+  },
+  {
+    "revision": "969f6df40dfa4a231b6d",
+    "url": "/Jags-byte/first-app/static/js/runtime-main.b6bef42b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Jags-byte/first-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Jags-byte/react/precache-manifest.c2efeaa95418fa8e3d448f03f4b2a317.js"
+  "/Jags-byte/first-app/precache-manifest.bf9299fc4520921b732f45ba4ad16239.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jags-byte/react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jags-byte/first-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
