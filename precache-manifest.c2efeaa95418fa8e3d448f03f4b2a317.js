2fcf9448fb5245158873ad2602a79287aa7d787c
Jags <jagadeeshmurali@gmail.com>
2020-07-28 00:38:31 +0530

    Updates

---
+++ precache-manifest.c2efeaa95418fa8e3d448f03f4b2a317.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372db2c2d084c60a83fb07f726bf3cfb",
+    "url": "/Jags-byte/react/index.html"
+  },
+  {
+    "revision": "c9fb8ed8740364fa0ffe",
+    "url": "/Jags-byte/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49b5286d487094d1a669",
+    "url": "/Jags-byte/react/static/js/2.b8eb2fb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jags-byte/react/static/js/2.b8eb2fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fb8ed8740364fa0ffe",
+    "url": "/Jags-byte/react/static/js/main.594998ce.chunk.js"
+  },
+  {
+    "revision": "40080b7f2f8683571e88",
+    "url": "/Jags-byte/react/static/js/runtime-main.61999be2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Jags-byte/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
